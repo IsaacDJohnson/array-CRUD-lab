d8b9e8860ebd70101931e2375041a9ca4bff9311
Isaacjohnson <isaac.designws@gmail.com>
2021-01-21 15:33:34 -0600

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["fire", "water", "earth", "air"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = []
+  array << "arrays!"
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = []
+  array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = "am"
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = "arrays!"
 end
